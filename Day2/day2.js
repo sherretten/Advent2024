b0d4637da78f6a95527bce821f25bbdc4c0de97c
Nate <nathaniels2016@gmail.com>
2024-12-05 19:01:10 -0800

    day 3

--- Day2/day2.js
+++ Day2/day2.js
@@ -1,19 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
+const fs_1 = require("fs");
 function retrieveReports() {
-    var file = (0, fs_1.readFileSync)("./input-day2.txt", "utf-8");
+    const file = (0, fs_1.readFileSync)("./input-day2.txt", "utf-8");
     return file;
 }
 function main() {
-    var reports = retrieveReports();
-    var safeReports = 0;
-    reports.split("\n").forEach(function (report) {
-        var row = report.split(" ").map(function (i) { return +i; });
-        var increasing = row[1] > row[0];
-        var numberOfErrors = 0;
-        for (var index = 0; index < row.length; index++) {
-            var indexDifference = row[index] - row[index + 1];
+    const reports = retrieveReports();
+    let safeReports = 0;
+    reports.split("\n").forEach((report) => {
+        let row = report.split(" ").map((i) => +i);
+        let increasing = row[1] > row[0];
+        let numberOfErrors = 0;
+        for (let index = 0; index < row.length; index++) {
+            let indexDifference = row[index] - row[index + 1];
             if (Math.abs(indexDifference) > 3) {
                 return;
             }
